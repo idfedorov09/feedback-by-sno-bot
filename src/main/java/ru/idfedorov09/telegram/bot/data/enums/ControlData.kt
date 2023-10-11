4ef1c774e8dd18ef7782b80caa03d506e2dbe932
Ivan Fedorov <idfedorov09@gmail.com>
2023-10-11 09:17:46 +0300

    settings

--- src/main/java/ru/idfedorov09/telegram/bot/data/enums/ControlData.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/data/enums/ControlData.kt
@@ -2,5 +2,5 @@ package ru.idfedorov09.telegram.bot.data.enums
 
 object ControlData {
     const val CREATOR_ID = "920061911"
-    const val ADMINS_CHAT_ID = "-1001788659890"
+    const val ADMINS_CHAT_ID = "-1001813732889"
 }
--- src/main/java/ru/idfedorov09/telegram/bot/flow/ExpContainer.kt
+++ src/main/java/ru/idfedorov09/telegram/bot/flow/ExpContainer.kt
@@ -1,7 +1,5 @@
 package ru.idfedorov09.telegram.bot.flow
 
-import ru.idfedorov09.telegram.bot.data.enums.BotStage
-
 /**
  * Объект контекста флоу, содержащий информацию о работающих фичах, режимах и тд и тп
  */
